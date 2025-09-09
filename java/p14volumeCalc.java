db6d7fbadf0d0ad5d5a09f9117f9332e561a25d9
Muḥammed Navār <navarmp@gmail.com>
2025-09-09 12:35:00 +0530

    Refactor volume and shape calculation classes
    Corrected method implementations and class structure in p14volumeCalc.java, fixing parameter names and logic for volume calculations. Cleaned up p15abstractClassShape.java by removing misplaced import statements, fixing class and method definitions, correcting string literals, and improving code formatting for clarity and correctness.

--- java/p14volumeCalc.java
+++ java/p14volumeCalc.java
@@ -1,41 +1,35 @@
-public class p14volumeCalc {
-    import java.util.*; 
-class Overload 
-{ 
-double area(float l,float w,float b) 
-{ 
-return l*w*h; 
-} 
-double area(float l) 
-{ 
-return l*l*l; 
-} 
-double area(float r,float h) 
-{ 
-return 3.14*r*r*r; 
-} 
-} 
- 
-{ 
-public static void main(String args[]) 
-{ 
-Overload ov=new Overload(); 
-Scanner sc=new Scanner(System.in); 
-System.out.println("Enter the length, width and height of the rectangular box"); 
-float l=sc.nextInt(); 
-float w=sc.nextInt(); 
-float h=sc.nextInt(); 
-double rect=ov.area(l,w,h); 
-System.out.println("Volume of rectangular box " + rect); 
-System.out.println("Enter the edge length of Cube"); 
-float e=sc.nextInt(); 
-double cube=ov.area(e); 
-System.out.println("Volume of Cube is " + cube); 
-System.out.println("Enter the radius and height of the Cylinder"); 
-float r=sc.nextInt(); 
-float hi=sc.nextInt(); 
-double cyli=ov.area(r,hi); 
-System.out.println("Volume of Cylinder is " + cyli); 
-} 
+import java.util.*; 
+class Overload {
+    double area(float l,float w,float b) {
+        return l*w*b;
+    }
+    double area(float l) { 
+        return l*l*l;
+    }
+    double area(float r,float h) {
+        return 3.14*r*r*h;
+    }
 }
+public class p14volumeCalc {
+    public static void main(String args[]) { 
+        Overload ov=new Overload(); 
+        Scanner sc=new Scanner(System.in); 
+        System.out.println("Enter the length, width and height of the rectangular box");
+
+        float l=sc.nextInt(); 
+        float w=sc.nextInt(); 
+        float h=sc.nextInt(); 
+        double rect=ov.area(l,w,h); 
+        System.out.println("Volume of rectangular box " + rect);
+
+        System.out.println("Enter the edge length of Cube"); 
+        float e=sc.nextInt(); 
+        double cube=ov.area(e); 
+        System.out.println("Volume of Cube is " + cube); 
+        System.out.println("Enter the radius and height of the Cylinder");
+        float r=sc.nextInt(); 
+        float hi=sc.nextInt(); 
+        double cyli=ov.area(r,hi); 
+        System.out.println("Volume of Cylinder is " + cyli); 
+        }
 }
