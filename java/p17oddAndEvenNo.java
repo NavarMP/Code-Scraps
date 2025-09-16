4715667f5b1e38b6404469c751bcfd7b07092cb2
Muḥammed Navār <navarmp@gmail.com>
2025-09-16 12:27:28 +0530

    Refactor odd and even number threading logic
    Cleaned up formatting and fixed string quotes in Even and Odd classes. Improved code readability and structure in p17oddAndEvenNo.java, ensuring proper thread creation and user input handling.

--- java/p17oddAndEvenNo.java
+++ java/p17oddAndEvenNo.java
@@ -1,77 +1,63 @@
-import java.io.*; 
+import java.io.*;
+import java.util.Scanner;
+
 class Even implements Runnable { 
     Thread even; 
     int limit,i; 
     Even(int a) { 
-        even=new Thread(this,“Even”); 
+        even=new Thread(this,"Even"); 
         limit=a; 
         even.start(); 
     } 
-    public void run() 
-    { 
-    try 
-    { 
-    for(i=2;i<limit;i=i+2) 
-    { 
-    System.out.println(i); 
-    Thread.sleep(500); 
-    } 
-    } 
-    catch(Exception e) 
-    { 
-    System.out.println(e); 
-    } 
-    } 
-    } 
+    public void run() { 
+        try { 
+            for(i=2;i<limit;i=i+2) { 
+                System.out.println(i); 
+                Thread.sleep(500); 
+            } 
+        } 
+        catch(Exception e) { 
+            System.out.println(e); 
+        } 
+    }
+} 
      
-    class Odd implements Runnable 
-    { 
+class Odd implements Runnable { 
     Thread odd; 
     int limit,i; 
-    Odd(int b) 
-    { 
-    odd=new Thread(this,“Odd”); 
-    limit=b; 
-    odd.start(); 
-    } 
-    public void run() 
-    { 
-    try 
-    { 
-    for(i=1;i<limit;i=i+2) 
-    { 
-    System.out.println(“\t” + i); 
-    Thread.sleep(500); 
-    } 
-    } 
-    catch(Exception e) 
-    { 
-    System.out.println(e); 
-    } 
-    } 
-    } 
-     
-public class p17oddAndEvenNo {
-    { 
-    public static void main(String args[]) throws IOException 
-    { 
-    int n1,n2; 
-    Scanner sc=new Scanner(System.in); 
-    System.out.println(“Enter limit of even numbers”); 
-    n1=sc.nextInt(); 
-    System.out.println(“Enter limit of odd numbers”); 
-    n2=sc.nextInt(); 
-    Even en=new Even(n1); 
-    Odd od=new Odd(n2); 
-    try 
-    { 
-    Thread.sleep(10000); 
-    } 
-    catch(Exception e) 
-    { 
-    System.out.println(e); 
-    } 
-    } 
+    Odd(int b) { 
+        odd=new Thread(this,"Odd"); 
+        limit=b; 
+        odd.start(); 
     }
-import java.util.Scanner;
+    public void run() { 
+        try { 
+            for(i=1;i<limit;i=i+2) { 
+                System.out.println("\t" + i); 
+                Thread.sleep(500); 
+            } 
+        } 
+        catch(Exception e) { 
+            System.out.println(e); 
+        } 
+    } 
+} 
+
+public class p17oddAndEvenNo { 
+    public static void main (String args[]) throws IOException { 
+        int n1,n2; 
+        Scanner sc=new Scanner(System.in); 
+        System.out.println("Enter limit of even numbers"); 
+        n1=sc.nextInt(); 
+        System.out.println("Enter limit of odd numbers"); 
+        n2=sc.nextInt(); 
+        Even en=new Even(n1); 
+        Odd od=new Odd(n2); 
+        try { 
+            Thread.sleep(10000); 
+        }
+        catch(Exception e) { 
+            System.out.println(e); 
+        }
+    }
 }
